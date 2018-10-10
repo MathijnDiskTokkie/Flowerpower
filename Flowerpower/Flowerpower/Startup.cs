70f1d398585b2896c1c86dac0b1a010331d423e8
Piratheepa <psatheeskumar01@student.rocvantwente.nl>
2018-10-10 12:57:26 +0200

    begin

---
+++ Flowerpower/Flowerpower/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Flowerpower.Startup))]
+namespace Flowerpower
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
