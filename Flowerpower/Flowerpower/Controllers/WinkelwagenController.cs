31baefa184a94bb72e6dfff045a57b8c45195401
MathijnDiskTokkie <msmits02@student.rocvantwente.nl>
2018-10-12 12:26:38 +0200

    winkelwagencontroller

---
+++ Flowerpower/Flowerpower/Controllers/WinkelwagenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Flowerpower.Controllers
+{
+    public class WinkelwagenController : Controller
+    {
+        // GET: Winkelwagen
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
