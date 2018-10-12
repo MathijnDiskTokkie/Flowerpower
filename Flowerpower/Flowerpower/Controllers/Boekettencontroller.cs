db272bcb7dc8860a58134c642a339525166c2933
MathijnDiskTokkie <msmits02@student.rocvantwente.nl>
2018-10-12 11:52:11 +0200

    boeketten toevoegen en boekingen controller

---
+++ Flowerpower/Flowerpower/Controllers/Boekettencontroller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Flowerpower.Models;
+
+namespace Flowerpower.Controllers
+{
+    public class Boekettencontroller : Controller
+    {
+        private FlowerpowerEntities db = new FlowerpowerEntities();
+
+        // GET: Boekettencontroller
+        public ActionResult Index()
+        {
+            return View(db.producten.ToList());
+        }
+
+        // GET: Boekettencontroller/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            producten producten = db.producten.Find(id);
+            if (producten == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producten);
+        }
+
+        // GET: Boekettencontroller/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Boekettencontroller/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "productid,productnaam,prijs,productomschrijving")] producten producten)
+        {
+            if (ModelState.IsValid)
+            {
+                db.producten.Add(producten);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(producten);
+        }
+
+        // GET: Boekettencontroller/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            producten producten = db.producten.Find(id);
+            if (producten == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producten);
+        }
+
+        // POST: Boekettencontroller/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "productid,productnaam,prijs,productomschrijving")] producten producten)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producten).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(producten);
+        }
+
+        // GET: Boekettencontroller/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            producten producten = db.producten.Find(id);
+            if (producten == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producten);
+        }
+
+        // POST: Boekettencontroller/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            producten producten = db.producten.Find(id);
+            db.producten.Remove(producten);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
