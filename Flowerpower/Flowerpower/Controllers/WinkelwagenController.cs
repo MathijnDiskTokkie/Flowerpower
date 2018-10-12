bd0ccc33ae213830c91b66ca31d9cd756721ca5c
Piratheepa <psatheeskumar01@student.rocvantwente.nl>
2018-10-12 13:56:49 +0200

    ....

---
+++ Flowerpower/Flowerpower/Controllers/WinkelwagenController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Flowerpower.Models;
+
+namespace FlowerPower.Controllers
+{
+    public class CartController : Controller
+    {
+        FlowerpowerEntities db = new FlowerpowerEntities();
+
+        private List<producten> shoppingCartList = new List<producten>();
+
+        // GET: Cart
+        public ActionResult Index()
+        {
+            shoppingCartList.OrderByDescending(s => s.prijs);
+
+            return View(shoppingCartList);
+        }
+    }
+}
