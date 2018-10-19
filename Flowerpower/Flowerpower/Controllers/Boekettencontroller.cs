027e190071ef7eaffd16ad94bb648f8ee0678edf
MathijnDiskTokkie <msmits02@student.rocvantwente.nl>
2018-10-19 11:19:03 +0200

    fix fix fix

--- Flowerpower/Flowerpower/Controllers/Boekettencontroller.cs
+++ Flowerpower/Flowerpower/Controllers/Boekettencontroller.cs
@@ -47,7 +47,7 @@ namespace Flowerpower.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "productid,productnaam,prijs,productomschrijving,gearchiveerd")] producten producten)
+        public ActionResult Create([Bind(Include = "productid,productnaam,prijs,productomschrijving,gearchiveerd, url")] producten producten)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace Flowerpower.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "productid,productnaam,prijs,productomschrijving,gearchiveerd")] producten producten)
+        public ActionResult Edit([Bind(Include = "productid,productnaam,prijs,productomschrijving,gearchiveerd, url")] producten producten)
         {
             if (ModelState.IsValid)
             {
